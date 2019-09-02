09437f8fe0925a4eb20c4b6c220e0f622ee26851
Benjamin Johnson <42893476+bmjcode@users.noreply.github.com>
2019-09-02 14:45:30 -0400

    Update the module docstring

--- userpaths/__init__.py
+++ userpaths/__init__.py
@@ -2,29 +2,29 @@
 
 The userpaths module provides cross-platform access to a user's special
 folders (or directories) like My Documents, Desktop, and Application Data.
+See the function list below for available paths.
 
-On Windows, it uses SHGetFolderPath() from shell32.dll. On other platforms,
-it provides the closest standard or generally-used de facto equivalent.
-
-The API is based on Ryan Ginstrom's winpaths [1], an existing module that
-provides a Windows-specific solution to special folder access. However,
-userpaths has a different overall focus (cross-platform access to a subset
-of user folders), and is not intended as an exact drop-in replacement.
-
-References:
-[1] http://ginstrom.com/code/winpaths.html
+Note: The userpaths module was originally designed from a Windows-centric
+perspective. Because of the many differences between the two systems, there
+are some Windows paths that do not have a direct equivalent on Unix, and
+vice versa. In these cases, userpaths attempts to return the nearest
+functional equivalent, but it is up to the user to ensure their application
+is using the appropriate path for what it seeks to do.
 """
 
 import sys
 
+# Import the appropriate implementation for the current system
 if sys.platform.startswith("win"):
     from .windows import *
 else:
     # Presume anything else is a Unix-like system
     from .unix import *
 
-# Synonym for get_my_documents()
+
+# Synonym for get_my_documents() for compatibility with winpaths
 get_personal = get_my_documents
+
 
 # These functions are defined in the individual platform modules
 __all__ = [
@@ -33,9 +33,9 @@ __all__ = [
     "get_downloads",
     "get_local_appdata",
     "get_my_documents",
-    "get_my_music",         # not in winpaths
+    "get_my_music",
     "get_my_pictures",
-    "get_my_videos",        # not in winpaths
+    "get_my_videos",
     "get_personal",
-    "get_profile",          # not in winpaths
+    "get_profile",
 ]
